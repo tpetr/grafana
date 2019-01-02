44786b8c675c94ef234804ed8bb963905138a3e4
tpetr <tpetr@hubspot.com>
2019-01-02 12:57:23 -0500

    fix

--- pkg/services/sqlstore/sqlstore.go
+++ pkg/services/sqlstore/sqlstore.go
@@ -210,7 +210,7 @@ func (ss *SqlStore) buildConnectionString() (string, error) {
 			protocol = "unix"
 		}
 
-		cnnstr = fmt.Sprintf("%s:%s@%s(%s)/%s?collation=utf8mb4_unicode_ci&allowNativePasswords=true&allowCleartextPassword=true",
+		cnnstr = fmt.Sprintf("%s:%s@%s(%s)/%s?collation=utf8mb4_unicode_ci&allowNativePasswords=true",
 			ss.dbCfg.User, ss.dbCfg.Pwd, protocol, ss.dbCfg.Host, ss.dbCfg.Name)
 
 		if ss.dbCfg.SslMode == "true" || ss.dbCfg.SslMode == "skip-verify" {
